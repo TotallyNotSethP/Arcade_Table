3bca17fcb79fc88864b6dde6feeecd9b7f674b6e
Seth Peace <sethevanpeace@gmail.com>
2019-05-11 14:06:39 -0700

    Added Black Joystick Identifier (Not Functioning)

--- Video_Game_Table.py
+++ Video_Game_Table.py
@@ -27,13 +27,35 @@ pygame.joystick.init()
 numJoysticks = pygame.joystick.get_count()
 
 # Alert The User That The Program Has Started
-info("The program has started.")
+info("Program Initiated")
 
 # Check That There Are 2 Joysticks Connected
-if(numJoysticks < 2):
-    error("Not enough joysticks connected. Expected: 2, Connected: {}.".format(numJoysticks))
-elif(numJoysticks > 2):
-    error("Too many joysticks connected. Expected: 2, Connected: {}.".format(numJoysticks))
+if(numJoysticks != 2):
+    error("Incorrect Joystick Number. Expected: 2, Connected: {}".format(numJoysticks))
 else:
-    info("2 joysticks connected.")
-    
+    info("2 Joysticks Connected")
+
+    # Create Joystick Objects
+    js0 = pygame.joystick.Joystick(0)
+    js0.init()
+    info("Joystick 0: " + str(js0))
+    js1 = pygame.joystick.Joystick(1)
+    js1.init()
+    info("Joystick 1: " + str(js1))
+
+    # Check Which Joystick Is Black
+    if(js0.get_button(2) == True):
+        info("Joystick 0: Black Joystick")
+        info("Joystick 1: White Joystick")
+        black = js0
+        white = js1
+    elif(js1.get_button(2) == True):
+        info("Joystick 0: Joystick")
+        info("Joystick 1: Black Joystick")
+        white = js0
+        black = js1
+    else:
+        error("No Signal on Black Joystick, Button Port 2")
+
+# Alert The User The Program Has Ended
+info("Program Ended")
